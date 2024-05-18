4f518b046397b99cedf6f2955145852c768d495e
Leo <124378386+leonel009@users.noreply.github.com>
2024-05-18 10:55:16 -0300

    Add files via upload

---
+++ Js/psicologia.js
@@ -0,0 +1,42 @@
+
+
+window.onload = function (){
+    
+    const destino = document.querySelector('#destino')
+
+    obtenerLibrosPsi();
+    
+
+}
+
+function construirLibros(libros){
+
+    destino.innerHTML = ''; // limpiar campo
+    
+    for (let libro of libros){ // For que va insertando codigo
+        destino.innerHTML +='<div class="tarjeta">'
+        destino.innerHTML +='<div class="imagen">'
+        destino.innerHTML +='<img src=' + libro.poster + ' alt="" class="imagen">'
+        destino.innerHTML +='</div>'
+        destino.innerHTML +='<div class="contenido">'   
+        destino.innerHTML +='<ul class="datos">'
+        destino.innerHTML +='<li><span class="titulo"> ' + libro.titulo + '</span><li>'
+        destino.innerHTML +='<li>Genero: <span>' + libro.genero + '</span></li>'
+        destino.innerHTML +='<li><span>Descripcion:</span> ' + libro.descripcion + '</li>'
+        destino.innerHTML +='<li>Precio: <span>' + libro.precio + ' ARS </span></li>'
+        destino.innerHTML +='</ul>'
+        destino.innerHTML +='</div>'
+        destino.innerHTML +='</div>'
+        console.log("hecho")
+    }
+}
+
+function obtenerLibrosPsi(){
+    fetch('../../libros/psicologia.json')
+    .then(res => res.json()) // el método .json() analiza la respuesta JSON en un objeto literal JS
+    .then(datos => {
+        console.log(datos)
+        construirLibros(datos) // insertar libros
+    })
+    
+}
